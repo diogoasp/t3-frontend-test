cad24e7d7a9ee94d8ba38c7d0817f43a31fd06da
daniellydsa <dsa.danielly15@gmail.com>
2022-10-30 00:43:35 -0300

    fix things

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -53,7 +53,7 @@ const Header = ({ user }: HeaderProps) => {
             <Navbar.Collapse className="justify-content-end">
               <Navbar.Text>
                 {hasUser() ? <span>Olá, {user?.email}</span> : null}
-                <Button variant='dark' data-testid="sair" onClick={toLogin()} >  {hasUser() ? <span className="danger">Sair</span> : <span>Entrar</span>} </Button>
+                <Button variant='dark' data-testid="sair" onClick={toLogin()} >  {hasUser() ? <span>Sair</span> : <span>Entrar</span>} </Button>
               </Navbar.Text>
             </Navbar.Collapse>
           </Container>
